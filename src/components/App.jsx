9b3f1580e32da1b1fe9c232b206bd428fff4793c
Halyna-UA <81983959+Halyna-UA@users.noreply.github.com>
2022-02-02 00:33:01 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,7 @@ import Feedback from '../components/Feedback/Feedback';
 export default function App() {
   return (
     <>
+    //повертає розмітку з Feedback //
       <h1 class="Feedback__title">Please leave feedback</h1>
       <Feedback/>
     </>
